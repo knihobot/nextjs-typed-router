a21d8019bdf1b1631b7f27f1ae0b0737aa6de66f
Vladimír Kopejska <kopejska.vladimir@gmail.com>
2022-12-12 20:01:09 +0100

    feat: add typescript, next and dependencies

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.example = void 0;
+function example() {
+    return "hello world";
+}
+exports.example = example;
